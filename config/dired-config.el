02a93cfd4155ad9bf3a6b976c5ae012025216291
vRzd <volodymyr.drozd.05@gmail.com>
2024-09-10 15:18:46 -0400

    Re-add init.el after clearing cache

--- config/dired-config.el
+++
@@ -1,9 +0,0 @@
-;; ============================
-;; DIRED MODE 
-;; ============================
-
-(add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
-
-(add-hook 'dired-mode-hook #'dired-hide-details-mode)
-
-(setq delete-by-moving-to-trash t)
